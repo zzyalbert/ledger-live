d610adbe23af4c804a415036e3921ba7b5aa1863
Gabriel R. Soares <gabriel.soares@ledger.fr>
2022-06-09 10:28:23 +0200

    First poc of image processing using webview on NFTs

--- apps/ledger-live-mobile/src/components/Nft/NftImageViewer.tsx
+++ apps/ledger-live-mobile/src/components/Nft/NftImageViewer.tsx
@@ -1,8 +1,7 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useRef, useState } from "react";
 import { View, StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 import NftImage from "./NftImage";
-
-// import PanAndZoomView from "../PanAndZoomView";
 
 type Props = {
   route: {
@@ -15,24 +14,117 @@ type RouteParams = {
   status: string;
 };
 
+
+const processImageCode = `
+const processImage = (imgBase64) => {
+    var image = new Image();
+
+    image.onload = () => {
+        var canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+    
+        var context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
+    
+        var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+        var x = 0;
+        var y = 0;
+
+        const imageDataGrayScale = [];
+
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            let gray = (0.299*imageData.data[i] + 0.587*imageData.data[i + 1] + 0.114*imageData.data[i + 2]);
+            // grayscale
+            gray = Math.floor(gray / 16) * 16;
+            // color reduction
+            imageDataGrayScale.push(gray);
+            imageDataGrayScale.push(gray);
+            imageDataGrayScale.push(gray);
+            // push 3 bytes for color (all the same == gray)
+
+            imageDataGrayScale.push(255);
+            // push alpha = max = 255
+        }
+        
+        var grayCanvas = document.createElement('canvas');
+
+        grayCanvas.width = image.width;
+        grayCanvas.height = image.height;
+
+        const grayData = Uint8ClampedArray.from(imageDataGrayScale);
+
+        var grayContext = grayCanvas.getContext('2d');
+        
+        grayContext.putImageData(new ImageData(grayData, image.width, image.height), 0, 0);
+
+        const grayScaleBase64 = grayCanvas.toDataURL();
+        window.ReactNativeWebView.postMessage(grayScaleBase64);
+    };
+
+    image.src = imgBase64;
+};`;
+
 const NftViewer = ({ route }: Props) => {
-  // T
   const { params } = route;
+
+  const [loaded, setLoaded] = useState(false);
+  const [grayScaleBase64, setGrayScaleBase64] = useState<string | null>(null);
+  const webViewRef = useRef<WebView>(null);
+    
+  useEffect(() => {
+    if(loaded || !params?.media || !webViewRef.current) return;
+
+    fetch(params.media)
+      .then(response => response.blob())
+      .then(data => new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onloadend = () =>  resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsDataURL(data);
+      }))
+      .then(data => {
+        console.log("Sending message");
+        webViewRef.current?.injectJavaScript(`
+          ${processImageCode}
+
+          processImage("${data}");
+        `);
+        setLoaded(true);
+      });
+
+  }, [loaded, webViewRef]);
+
+
+  
 
   return (
     <View style={styles.imageContainer}>
       <NftImage
-        src={params?.media}
+        src={grayScaleBase64 ?? params?.media}
         status={params?.status}
         style={styles.image}
         hackWidth={10000}
         resizeMode="contain"
       />
+      <WebView
+        androidLayerType="software"
+        ref={webViewRef}
+        androidHardwareAccelerationDisabled={true}
+        style={styles.webview}
+        onMessage={e => {
+          setGrayScaleBase64(e.nativeEvent.data);
+        }} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  webview: {
+    opacity: 0.99,
+    overflow: "hidden",
+  },
   imageContainer: {
     flex: 1,
     justifyContent: "center",
